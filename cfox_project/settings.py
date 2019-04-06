20b23f86e6f0d7abbaa0be45949df71551b9f569
fuyanbin <been1986@gmail.com>
2019-04-06 15:15:47 +0800

    add database config

--- cfox_project/settings.py
+++ cfox_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -78,10 +80,7 @@ WSGI_APPLICATION = 'cfox_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': config.DATABASE_CONFIG
 }
 
 
---
+++ config.py
@@ -0,0 +1,16 @@
+#! -*- coding: utf-8 -*-
+
+# database
+DATABASE_NAME = 'cfox_database_qs4ZElYW'
+DATABASE_HOST = '127.0.0.1'
+DATABASE_PORT = 3306
+DATABASE_USER = 'fyb01'
+DATABASE_PASSWORD = 'i9Vofnmjk'
+DATABASE_CONFIG = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': DATABASE_NAME,
+    'USER': DATABASE_USER,
+    'PASSWORD': DATABASE_PASSWORD,
+    'HOST': DATABASE_HOST,
+    'PORT': DATABASE_PORT
+}

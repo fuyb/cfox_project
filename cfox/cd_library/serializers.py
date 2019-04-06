ad5b3dcf544237de9958ee0a368adcd1a627d5d0
fuyanbin <been1986@gmail.com>
2019-04-06 20:21:19 +0800

    use dynamic fields

--- cfox/cd_library/serializers.py
+++ cfox/cd_library/serializers.py
@@ -1,23 +1,24 @@
 #! -*- coding: utf-8 -*-
 
 from rest_framework import serializers
+from drf_queryfields import QueryFieldsMixin
 
 from .models import *
 
-class ArtistSerializer(serializers.ModelSerializer):
+class ArtistSerializer(QueryFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = Artist
         fields = ('pk', 'name', 'birthday', 'date_death', 'description')
 
 
-class AlbumSerializer(serializers.ModelSerializer):
+class AlbumSerializer(QueryFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = Album
         fields = ('pk', 'title', 'artist', 'date_published', 'genre', 'cover', 
                   'date_modified', 'created')
 
 
-class MusicSerializer(serializers.ModelSerializer):
+class MusicSerializer(QueryFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = Music
         fields = ('pk', 'title', 'artist', 'album', 'date_published', 'genre',

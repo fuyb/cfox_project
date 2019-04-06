8570bc391886cd2583b101a967db2aaf786c161d
fuyanbin <been1986@gmail.com>
2019-04-06 17:23:41 +0800

    typo

--- core/models.py
+++ core/models.py
@@ -1,13 +1,13 @@
 #! -*- coding: utf-8 -*-
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 class BaseManager(models.Manager):
     def get_queryset(self):
-        return supert().get_queryset().filter(deleted=False)
+        return super().get_queryset().filter(deleted=False)
 
 class BaseModel(models.Model):
     created = models.DateTimeField(_(u'创建时间'), auto_now_add=True)
